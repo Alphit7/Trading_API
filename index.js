7f01c90e6ed040afe2547f2c9c0518d03f66d078
Alphit7 <jacobs.sylvain@gmail.com>
2023-08-22 09:39:56 +0200

    installed some things

---
+++ index.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const app = express();
+const PORT = 8080;
+
+app.use( express.json() )
+
+app.listen(
+    PORT,
+    () => console.log(`it's alive on http://localhost:${PORT}`)
+    )
+
+    app.get('/hello', (req,res) => {
+        res.status(200).send({
+            message: "Hello"
+        })
+    });
+
+    app.post('/hello/:id', (req,res) => {
+        const{message} = req.body;
+        res.send({
+            message: `Message '${message}' added`
+        })
+    });
